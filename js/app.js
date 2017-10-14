df9c0c01551b9c8c6c1b43c2ada7500966296a4b
nasserfj58 <nasserfj58@gmail.com>
2017-10-14 12:03:31 +0300

    adding main page

--- js/app.js
+++ js/app.js
@@ -1,6 +1,27 @@
+Traveler = function(username,email,password,photoPath,socialm,Fname,Lname,intrsts){
+  var self = this;
+  self.Fname = ko.observable(Fname);
+  self.email = ko.observable(email);
+  self.Lname = ko.observable(Lname);
+  self.photoPath = ko.observable(photoPath);
+  self.password = ko.observable(password);
+  self.username = ko.observable(username);
+  self.intrests = ko.observableArray(intrsts);
+}
 
+Trip = function(date,destnation,description,budjet,activities,travelers,createdBy){
+  var self = this;
+  self.date = ko.observable(date);
+  self.destnation = ko.observable(destnation);
+  self.description = ko.observable(description);
+  self.budjet = ko.observable(budjet);
+  self.activities = ko.observable(activities);
+  self.createdBy = ko.observable(createdBy);
+  self.travelers = ko.observableArray(travelers);
 
-ViewModel=function() {
+}
+
+ViewModel = function() {
   var self = this;
   //this.map = ko.observable(new map(-25.363,131.044));
   self.locations=ko.observableArray();
